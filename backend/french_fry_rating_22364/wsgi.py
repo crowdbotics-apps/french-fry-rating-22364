a7bb056abf66625eb55c768025db2d702aa77559
Crowdbotics <team@crowdbotics.com>
2020-11-06 23:53:25 +0000

    Update models

--- backend/french_fry_rating_22364/wsgi.py
+++ backend/french_fry_rating_22364/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'french_fry_rating_22364.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "french_fry_rating_22364.settings")
 
 application = get_wsgi_application()
